8fe86f4701e2ff0409c0fd5f85342ea3b3f2c94f
Cloudtemplar26 <fredrichtan@icloud.com>
2024-11-23 03:58:48 -0500

    completed 2.2 all endpoint test runs smoothly with dockerized settings

---
+++ init/mongo/mongo-init.js
@@ -0,0 +1,7 @@
+db = db.getSiblingDB('product-service');
+db.createUser({
+    user: "admin",
+    pwd: "password",
+    roles: [{ role: "readWrite", db: "product-service" }]
+});
+db.createCollection('products');
